4c2016dfe76a2fc67ee8e9d2da65c16f251561a8
Hyacinthe396 <hmutoni520@daviscollege.com>
2021-04-23 16:06:28 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VsCodeLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y =7;
+            Console.WriteLine(x+y);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello everyOne");
+
+            const string Fname = "Hyacinthe";
+            const string Lname = "Mutoni";
+            Console.WriteLine(Fname + " "+ Lname);
+
+            Console.WriteLine("Enter your name please!");
+            string Mynames = Console.ReadLine();
+            Console.Write(Mynames);
+
+        }
+    }
+}
